f593d884dddeb213dd2dec9852c6c8e9d9db2ed7
Lee Phillips <leegphillips@users.noreply.github.com>
2016-04-28 20:01:20 +0100

    Update encrypt2Dropbox.sh

--- encrypt2Dropbox.sh
+++ encrypt2Dropbox.sh
@@ -1,4 +1,2 @@
-rm -rf ~/tmp/*
-tar -zcvf ~/tmp/Documents.tar.gz -C ~ Documents
-openssl enc -aes-256-cbc -salt -in ~/tmp/Documents.tar.gz -out ~/tmp/Documents.tar.gz.enc
-cp ~/tmp/Documents.tar.gz.enc ~/Dropbox
+tar -czf - ~/Documents/ | openssl enc -aes-256-cbc -salt -out ~/Documents.tar.gz.enc
+cp ~/Documents.tar.gz.enc ~/Dropbox
