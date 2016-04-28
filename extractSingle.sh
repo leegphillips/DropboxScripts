78ac8568f29a425b1461223f71a70ba02cfef0d7
Lee Phillips <leegphillips@users.noreply.github.com>
2016-04-28 20:24:04 +0100

    Update extractSingle.sh

--- extractSingle.sh
+++ extractSingle.sh
@@ -1,1 +1,1 @@
-openssl enc -d -aes-256-cbc -in Documents.tar.gz.enc | tar xvf - -C . Documents/invoices/Invoice1.xls
+openssl enc -d -aes-256-cbc -in Documents.tar.gz.enc | tar xvf - --strip-components 2 -C . Documents/invoices/Invoice1.xls
