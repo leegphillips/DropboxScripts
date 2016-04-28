08f3f8ae5039174e4cf767a05b0abae39423d0a5
Lee Phillips <leegphillips@users.noreply.github.com>
2016-04-28 15:12:30 +0100

    Update decryptFromDropbox.sh

--- decryptFromDropbox.sh
+++ decryptFromDropbox.sh
@@ -1,3 +1,4 @@
-cp ~/Dropbox/Documents.tar.gz.enc ~/Documents.tar.gz.enc
-openssl enc -d -aes-256-cbc -in ~/Documents.tar.gz.enc
+rm -rf ~/tmp/*
+cp ~/Dropbox/Documents.tar.gz.enc ~/tmp/Documents.tar.gz.enc
+openssl enc -d -aes-256-cbc -in ~/tmp/Documents.tar.gz.enc -out /tmp/Documents.tar.gz
 tar -zxvf ~/Documents.tar.gz
