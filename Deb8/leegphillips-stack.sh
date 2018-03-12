5e7fb9fc9ff9ed416577b1718a2e3b4a5da60f91
Lee Phillips <leegphillips@users.noreply.github.com>
2018-03-12 13:26:54 +0000

    Update leegphillips-stack.sh

--- Deb8/leegphillips-stack.sh
+++ Deb8/leegphillips-stack.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
 apt-get update
-apt install -t jessie-backports openjdk-8-jre-headless ca-certificates-java
+apt -y install -t jessie-backports openjdk-8-jre-headless ca-certificates-java
 apt-get -y install openjdk-8-jdk curl maven
