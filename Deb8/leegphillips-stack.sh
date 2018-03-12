1792ecb83945ccc130e016ae72ed22083889deee
Lee Phillips <leegphillips@users.noreply.github.com>
2018-03-12 15:10:15 +0000

    Update leegphillips-stack.sh

--- Deb8/leegphillips-stack.sh
+++ Deb8/leegphillips-stack.sh
@@ -14,7 +14,7 @@ update-java-alternatives --set java-1.8.0-openjdk-amd64
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
-apt-get update && apt-get -y install kibana elasticsearch
+apt-get update && export ES_SKIP_SET_KERNEL_PARAMETERS=true && apt-get -y install kibana elasticsearch
 systemctl daemon-reload
 systemctl enable elasticsearch.service
 systemctl start elasticsearch.service
