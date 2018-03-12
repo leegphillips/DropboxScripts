f0e4db2a0392508c8de5cb507f0fa27cd451a234
Lee Phillips <leegphillips@users.noreply.github.com>
2018-03-12 10:45:55 +0000

    Update leegphillips-stack.sh

--- Deb8/leegphillips-stack.sh
+++ Deb8/leegphillips-stack.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/java-8-debian.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/java-8-debian.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 apt-get update
-apt-get -y install maven
+apt-get -y install oracle-java8-installer oracle-java8-set-default curl maven
