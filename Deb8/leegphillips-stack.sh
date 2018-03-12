21e7d0966e993dc601388a84c096d030d0308197
Lee Phillips <leegphillips@users.noreply.github.com>
2018-03-12 11:51:15 +0000

    Update leegphillips-stack.sh

--- Deb8/leegphillips-stack.sh
+++ Deb8/leegphillips-stack.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/java-8-debian.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/java-8-debian.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
 apt-get update
-apt-get -y install oracle-java8-installer oracle-java8-set-default curl maven
+apt install -t jessie-backports openjdk-8-jre-headless ca-certificates-java
+apt-get -y install openjdk-8-jdk curl maven
