3083f41fb00ac5c91313c61bdf7b677128e33bdc
Lee Phillips <leegphillips@users.noreply.github.com>
2018-03-14 16:31:11 +0000

    Update elk.sh

--- Deb8/ELK/elk.sh
+++ Deb8/ELK/elk.sh
@@ -7,20 +7,23 @@ set -x
 echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
 apt-get update
 apt -y install -t jessie-backports openjdk-8-jre-headless ca-certificates-java
-apt-get -y install openjdk-8-jdk curl maven git-core
+apt-get -y install openjdk-8-jdk curl maven git-core netcat
 update-java-alternatives --set java-1.8.0-openjdk-amd64
 
 #install ELK
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
-apt-get update && export ES_SKIP_SET_KERNEL_PARAMETERS=true && apt-get -y install kibana elasticsearch filebeat
+apt-get update && export ES_SKIP_SET_KERNEL_PARAMETERS=true && apt-get -y install kibana elasticsearch logstash filebeat
 wget -O /etc/elasticsearch/jvm.options https://raw.githubusercontent.com/leegphillips/Scripts/master/Deb8/ELK/elasticsearch-jvm-options
+wget -O /etc/logstash/jvm.options https://raw.githubusercontent.com/leegphillips/Scripts/master/Deb8/ELK/logstash-jvm-options
 systemctl daemon-reload
 systemctl enable elasticsearch.service
 systemctl start elasticsearch.service
 systemctl enable kibana.service
 systemctl start kibana.service
+systemctl enable logstash.service
+systemctl start logstash.service
 
 #install Nginx
 systemctl stop apache2.service
