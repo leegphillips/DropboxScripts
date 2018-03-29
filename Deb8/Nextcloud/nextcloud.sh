7e7e23f298e7089799e177d47950d7308e95576f
Lee Phillips <leegphillips@users.noreply.github.com>
2018-03-29 10:42:07 +0100

    Update nextcloud.sh

--- Deb8/Nextcloud/nextcloud.sh
+++ Deb8/Nextcloud/nextcloud.sh
@@ -6,3 +6,13 @@ set -x
 #set UK timezone
 mv /etc/localtime /etc/localtime.default
 cp /usr/share/zoneinfo/Europe/London /etc/localtime
+
+apt-get update -y
+apt-get upgrade -y
+
+echo 'deb http://apt.jurisic.org/debian/ jessie main contrib non-free' >> /etc/apt/sources.list.d/nextcloud.list
+wget -q http://apt.jurisic.org/Release.key -O- | apt-key add -
+apt-get update
+
+apt-get install postgresql -y
+
